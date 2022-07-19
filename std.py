9958737f4feab5b3e80ab564285e511a66fa70d5
Harry Chen <i@harrychen.xyz>
2022-07-19 17:26:53 +0800

    Refine std to be readable
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- std.py
+++ std.py
@@ -8,7 +8,8 @@ input_file = sys.argv[1]
 output_file = sys.argv[2]
 
 
-def convert(s):
+# convert description of week range to interval
+def to_interval(s):
     i = [0, 0]
     if s == u'全周':
         i = [1, 16]
@@ -23,42 +24,43 @@ def convert(s):
     return i
 
 
+# check whether two week ranges conflict
 def conflict(e, c):
-    ei = convert(e)
-    ci = convert(c)
+    ei = to_interval(e)
+    ci = to_interval(c)
     return not (ei[1] < ci[0] or ei[0] > ci[1])
 
 
 with open(input_file, 'r', encoding='utf-8') as f:
     lines = f.readlines()
     n, m = list(map(int, lines[0].rstrip('\n').split(' ')))
-    d = defaultdict(list)
+    time_slots = defaultdict(list)  # all available slots
+
+    # read selected courses
     for i in range(n):
         l = lines[1+i].rstrip('\n').split(' ')
-        num = int(l[1])
-        ind = int(l[3])
-        nam = l[2]
-        t = l[4].split(',')
-        for s in t:
-            k = s.split('(')[0]
-            r = s.split('(')[1].rstrip(')')
-            d[k].append((r, num, ind, nam))
+        num, index, name = int(l[1]), int(l[3]), l[2]
+        # process time list
+        for s in l[4].split(','):
+            slot = s.split('(')[0]
+            weeks = s.split('(')[1].rstrip(')')
+            time_slots[slot].append((weeks, num, index, name))
 
     with open(output_file, 'w', encoding='utf-8') as of:
+        # read candidate courses
         for i in range(m):
             l = lines[1+n+i].rstrip('\n').split(' ')
-            num = int(l[0])
-            ind = int(l[1])
-            nam = l[2]
-            t = l[5].split(',')
-            w = set()
-            for s in t:
-                k = s.split('(')[0]
-                r = s.split('(')[1].rstrip(')')
-                for c in d[k]:
-                    if conflict(r, c[0]):
-                        w.add(c[1:])
-            if len(w):
-                of.write(f'{num} {ind} {nam}:\n')
-                for t in sorted(w):
-                    of.write(f'\t{t[0]} {t[1]} {t[2]}\n')
+            num, index, name = int(l[0]), int(l[1]), l[2]
+            conflicts = set()
+            # check week range conflicts
+            for s in l[5].split(','):
+                slot = s.split('(')[0]
+                req_weeks = s.split('(')[1].rstrip(')')  # requested range
+                for curr_weeks, *info in time_slots[slot]:
+                    if conflict(req_weeks, curr_weeks):
+                        conflicts.add(tuple(info))
+            # output conflicts if not empty
+            if len(conflicts) != 0:
+                of.write(f'{num} {index} {name}:\n')
+                for info in sorted(conflicts):
+                    of.write(f'\t{"{} {} {}".format(*info)}\n')
